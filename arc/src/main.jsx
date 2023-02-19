969b7ec39d0aa00063594ec72eac0bad48a1a215
Raktim Shrestha <raktim@rumsan.com>
2023-02-19 22:10:34 +0545

    template change

---
+++ arc/src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import { AuthProvider } from "@arcana/auth";
+import { ProvideAuth } from "@arcana/auth-react";
+const APP_ID = import.meta.env.VITE_ARCANA_APP_ID;
+
+const provider = new AuthProvider(`${APP_ID}`, {
+  network: "testnet",
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ProvideAuth provider={provider}>
+      <App />
+    </ProvideAuth>
+  </React.StrictMode>
+);
